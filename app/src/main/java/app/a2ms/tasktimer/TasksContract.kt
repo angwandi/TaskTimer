426bcab58dc7334568ac83b0b4d40b4921c1664b
Innocent <31923567+angwandi@users.noreply.github.com>
2018-09-17 10:08:43 +0100

    Add TasksContract object

---
+++ app/src/main/java/app/a2ms/tasktimer/TasksContract.kt
@@ -0,0 +1,17 @@
+package app.a2ms.tasktimer
+
+import android.provider.BaseColumns
+
+object TasksContract {
+    //use internal access modifier so the  name of our table can only  be visible inside our package
+    //define the name of the table
+    internal const val TABLE_NAME = "Tasks" // we can use const inside an object but can't in class
+
+    //Tasks fields : {@TasksContract} constructor: to be used to refer to our table columns name
+    object Colums {
+        const val ID = BaseColumns._ID  //The unique ID for a row.
+        const val TASK_NAME = "Name"
+        const val TASK_DESCRIPTION = "Description"
+        const val TASK_SORT_ORDER = "SortOrder"
+    }
+}
