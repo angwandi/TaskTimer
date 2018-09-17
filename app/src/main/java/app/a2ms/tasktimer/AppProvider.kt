fd6f684e616638c503f8210a02f6a27868a99f77
Innocent <31923567+angwandi@users.noreply.github.com>
2018-09-17 18:36:46 +0100

    Add content provider update and delete
    Signed-off-by: Innocent <31923567+angwandi@users.noreply.github.com>

--- app/src/main/java/app/a2ms/tasktimer/AppProvider.kt
+++ app/src/main/java/app/a2ms/tasktimer/AppProvider.kt
@@ -124,7 +124,7 @@ class AppProvider : ContentProvider() {
             TASKS -> {
                 val db = AppDatabase.getInstance(context).writableDatabase
                 recordId = db.insert(TasksContract.TABLE_NAME, null, values)
-                if (recordId != -1L) { //check nullColumnHack above
+                if (recordId != -1L) { // to check nullColumnHack above
                     returnUri = TasksContract.buildUriFromId(recordId)
                 } else {
                     throw SQLException("Failed to insert, Uri was $uri")
@@ -133,7 +133,7 @@ class AppProvider : ContentProvider() {
             TIMINGS -> {
                 val db = AppDatabase.getInstance(context).writableDatabase
                 recordId = db.insert(TimingsContract.TABLE_NAME, null, values)
-                if (recordId != -1L) { //check nullColumnHack above
+                if (recordId != -1L) { //to check nullColumnHack above
                     returnUri = TimingsContract.buildUriFromId(recordId)
                 } else {
                     throw SQLException("Failed to insert, Uri was $uri")
@@ -146,10 +146,97 @@ class AppProvider : ContentProvider() {
     }
 
     override fun update(uri: Uri, values: ContentValues, selection: String?, selectionArgs: Array<out String>?): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Log.d(TAG, "update: called with uri $uri")
+        val match = uriMatcher.match(uri)
+        Log.d(TAG, "update: match is  $match")
+
+        val count: Int
+        var selectionCriteria: String
+
+        when (match) {
+            TASKS -> {
+                val db = AppDatabase.getInstance(context).writableDatabase
+                count = db.update(TasksContract.TABLE_NAME, values, selection, selectionArgs)
+            }
+            TASKS_ID -> {
+                val db = AppDatabase.getInstance(context).writableDatabase
+                val id = TasksContract.getId(uri)
+                selectionCriteria = "${TasksContract.Columns.ID}=$id"
+
+                if (selection != null && selection.isNotEmpty()) {
+                    selectionCriteria += "AND ($selection)"
+                }
+                count = db.update(TasksContract.TABLE_NAME, values, selectionCriteria, selectionArgs)
+            }
+            TIMINGS -> {
+                val db = AppDatabase.getInstance(context).writableDatabase
+                count = db.update(TimingsContract.TABLE_NAME, values, selection, selectionArgs)
+            }
+            TIMINGS_ID -> {
+                val db = AppDatabase.getInstance(context).writableDatabase
+                val id = TimingsContract.getId(uri)
+                selectionCriteria = "${TimingsContract.Columns.ID}=$id"
+
+                if (selection != null && selection.isNotEmpty()) {
+                    selectionCriteria += "AND ($selection)"
+                }
+                count = db.update(TimingsContract.TABLE_NAME, values, selectionCriteria, selectionArgs)
+            }
+            else -> {
+                throw java.lang.IllegalArgumentException("Unknown uri:$uri")
+            }
+        }
+        Log.d(TAG, "Existing update, returning $count")
+        return count
     }
 
+    /**
+     * Tips: delete code is pretty much the same as update :
+     * remove update and put delete
+     * also remove values as not needed for delete
+     * */
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Log.d(TAG, "delete: called with uri $uri")
+        val match = uriMatcher.match(uri)
+        Log.d(TAG, "delete: match is  $match")
+
+        val count: Int
+        var selectionCriteria: String
+
+        when (match) {
+            TASKS -> {
+                val db = AppDatabase.getInstance(context).writableDatabase
+                count = db.delete(TasksContract.TABLE_NAME, selection, selectionArgs)
+            }
+            TASKS_ID -> {
+                val db = AppDatabase.getInstance(context).writableDatabase
+                val id = TasksContract.getId(uri)
+                selectionCriteria = "${TasksContract.Columns.ID}=$id"
+
+                if (selection != null && selection.isNotEmpty()) {
+                    selectionCriteria += "AND ($selection)"
+                }
+                count = db.delete(TasksContract.TABLE_NAME, selectionCriteria, selectionArgs)
+            }
+            TIMINGS -> {
+                val db = AppDatabase.getInstance(context).writableDatabase
+                count = db.delete(TimingsContract.TABLE_NAME, selection, selectionArgs)
+            }
+            TIMINGS_ID -> {
+                val db = AppDatabase.getInstance(context).writableDatabase
+                val id = TimingsContract.getId(uri)
+                selectionCriteria = "${TimingsContract.Columns.ID}=$id"
+
+                if (selection != null && selection.isNotEmpty()) {
+                    selectionCriteria += "AND ($selection)"
+                }
+                count = db.delete(TimingsContract.TABLE_NAME, selectionCriteria, selectionArgs)
+            }
+            else -> {
+                throw java.lang.IllegalArgumentException("Unknown uri:$uri")
+            }
+        }
+        Log.d(TAG, "Existing update, returning $count")
+        return count
     }
 }
