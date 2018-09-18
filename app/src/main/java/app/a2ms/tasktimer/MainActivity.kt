b23d28e4d82498f4947f063e4fb67b0f36d0dc64
Innocent <31923567+angwandi@users.noreply.github.com>
2018-09-18 19:14:40 +0100

    Enable Multi-pane fragments
    Signed-off-by: Innocent <31923567+angwandi@users.noreply.github.com>

--- app/src/main/java/app/a2ms/tasktimer/MainActivity.kt
+++ app/src/main/java/app/a2ms/tasktimer/MainActivity.kt
@@ -1,5 +1,6 @@
 package app.a2ms.tasktimer
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
@@ -21,6 +22,23 @@ class MainActivity : AppCompatActivity(), AddEditFragment.OnSavedClicked {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        mTwoPane = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+        var fragment = supportFragmentManager.findFragmentById(R.id.task_details_container)
+        if (fragment != null) {
+            //There was an existing fragment to edit a task, make sure the panes are set correctly
+            showEditPane()
+        } else {
+            task_details_container.visibility = if (mTwoPane) View.INVISIBLE else View.GONE
+            mainFragment.view?.visibility = View.VISIBLE
+        }
+
+    }
+
+    private fun showEditPane() {
+        task_details_container.visibility = View.VISIBLE
+        //hide the left hand pane, if in single pane view
+        mainFragment.view?.visibility = if (mTwoPane) View.VISIBLE else View.GONE
     }
 
     private fun removeEditPane(fragment: Fragment? = null) {
@@ -66,6 +84,7 @@ class MainActivity : AppCompatActivity(), AddEditFragment.OnSavedClicked {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.task_details_container, newFragment)
                 .commit()
+        showEditPane()
         Log.d(TAG, "Exiting taskEditRequest")
     }
 }
